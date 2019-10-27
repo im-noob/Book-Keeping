c61646bb4a828b4febe6ddbaf0bf361b758b3669
amriteshonly4u <amriteshonly4u@gmail.com>
2019-10-27 18:54:04 +0530

    Upload CSV done

--- app/Http/Controllers/transactions_c.php
+++ app/Http/Controllers/transactions_c.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\transactions;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\File;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\Translation\Loader\CsvFileLoader;
 
 class transactions_c extends Controller
 {
@@ -46,9 +49,68 @@ class transactions_c extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $storagePath =  storage_path();
+        $this->validate($request,[
+            'file'=> 'required',
+        ]);
+        
+        $CSV_file = $request->file('file');
+        // $file_name = $CSV_file->getClientOriginalName();
+        $ext = strtolower($CSV_file->getClientOriginalExtension());
+
+        //checking extesion matched or not
+        if(strcmp("csv",$ext) == 0){
+            // Storing File 
+            $path = $CSV_file->storeAs('/','temp.csv');
+            
+            // //retrinving and reading... geting content 
+            // $content = Storage::get($path);
+
+            // //converting to csv array
+            // $transArr = str_getcsv($content,",","","\n");
+
+            // //extracting and pusshing in DB
+            // foreach ($transArr as $key => $value) {
+            //     // if ($key>3) {
+            //         echo($key."--->".$value."\n");
+            //     // }
+            // }
+            
+            $handle = fopen($storagePath ."/app/". $path,'r');
+            $arr = $this->csvToArr($handle);
+
+            // pushing to array
+            foreach ($arr as $key => $value) {
+                if($key){
+                    $transactions_var = new transactions();
+                    $transactions_var->from_user_id = $value[0];
+                    $transactions_var->to_user_id = $value[1];
+                    $transactions_var->transaction_amount = $value[2];
+
+                    // convertin date to time stamp
+                    $timestamp = \Carbon\Carbon::createFromFormat('d-m-Y', $value[3])->toDateTimeString();
+                    // var_dump($timestamp);
+                    $transactions_var->transaction_date = $timestamp;
+                    $transactions_var->save();
+                }
+            }
+            
+            return redirect()->route('transactions.index')->with('success','Successfully Uploaded');
+
+        }else{
+            echo "extesion mismatch";
+            return redirect()->route('uploadCSV')->with('failed','Invalid File Extension');
+        }
+
     }
 
+    public function csvToArr($handle){
+        $finalArr = [];
+        while(($row = fgetcsv($handle,1000)) !== false){
+            array_push($finalArr,$row);
+        }
+        return($finalArr);
+    }
     /**
      * Display the specified resource.
      *
--- resources/views/layouts/uploadCSV.blade.php
+++ resources/views/layouts/uploadCSV.blade.php
@@ -7,16 +7,21 @@
                     <h5 class="card-title">Upload CSV</h5>
                     <h6 class="card-subtitle mb-4 text-muted">(from_user_id, to_user_id, transaction_amount, transaction_date)</h6>
 
-                    <div class="input-group">
-                        <div class="custom-file">
-                            <input type="file" type=".csv" class="custom-file-input" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04">
-                            <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
+                    <form method="POST" action="{{url('transactions')}}" enctype="multipart/form-data">
+                        @csrf
+                        @method('POST')
+                        <div class="input-group">
+                            <div class="custom-file">
+                                <input type="file" name="file" type=".csv" class="custom-file-input" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04">
+                                <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
+                            </div>
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="submit" id="inputGroupFileAddon04">Upload File</button>
+                            </div>
                         </div>
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04">Upload File</button>
-                        </div>
-                    </div>
-                </div>
+                    </form>
+                    
+                </div> 
             </div>
     </section>
 @endsection
